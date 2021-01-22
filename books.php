5aabea75a21c4793c366930cf6cb2a9aa0f4d119
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-01-21 18:18:44 -0700

    latest

--- books.php
+++ books.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+
+echo '{"books-read":[{"Title":"Night","Author":"Elie Wiesel"},{"Title":"Girl, Stop Apologizing", "Author":"Rachel Hollis"}]}'
 ?>
 
-{"books-read":[{"Title":"Night","Author":"Elie Wiesel"},{"Title":"Girl, Stop Apologizing", "Author":"Rachel Hollis"}]}
