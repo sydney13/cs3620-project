0bc99d4f9e800f6b901a69c5d0215d54e8ee013c
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-01-28 18:23:26 -0700

    new

--- sqltest.php
+++ sqltest.php
@@ -1,7 +1,10 @@
 <?php
+
+session_start();
+
 $servername = "cs3620.mysql.database.azure.com";
-$username = $_ENV['sydneysorensen@cs3620'];
-$password = $_ENV['Volleyball13!'];
+$username = (isset($_SESSION['SQLUSR']) ? $_SESSION['SQLUSR'] : $_ENV['SLQUSER']);
+$password = (isset($_SESSION['SQLPW']) ? $_SESSION['SQLPW'] : $_ENV['SLQPW']);
 $dbname = "cs3620_proj";
 
 // Create connection
---
+++ test.php
@@ -0,0 +1,6 @@
+<?
+
+session_start();
+echo $_SESSION['SQLPW'];
+
+?>
