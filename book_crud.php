60eb7fc6db7fddcaacd9a53fecde7a6a4522ce13
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-03 13:56:32 -0700

    new

--- book_crud.php
+++ book_crud.php
@@ -28,6 +28,21 @@ if ($conn->query($sql) === TRUE) {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+  } else {
+    echo "Error deleting record: " . $conn->error;
+  }
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["bookid"]. " " . $row["booktitle"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+
 $conn->close();
 
 
