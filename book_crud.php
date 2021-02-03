591a8692807cc8c3c3f3286b171be2bc052f4bda
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-03 11:46:01 -0700

    new

---
+++ book_crud.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+$servername = "cs3620.mysql.database.azure.com";
+$username = (isset($_SESSION['SQLUSER']) ? $_SESSION['SQLUSER'] : $_ENV['SQLUSER']);
+$password = (isset($_SESSION['SQLPW']) ? $_SESSION['SQLPW'] : $_ENV['SQLPW']);
+$dbname = "cs3620_proj";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO book (bookid, booktitle)
+VALUES (1, 'Harry Potter')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+?>
