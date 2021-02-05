15aeb37a8a2ed41757792e8267d6b108e8d89f58
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-04 18:46:40 -0700

    new

---
+++ user/user.php
@@ -0,0 +1,19 @@
+<?php
+
+    class User {
+
+        public function getUser($user_id){
+
+        }
+        public function getUsername(){
+            return "SydneyWilkes22";
+        }
+        public function getFirstName(){
+            return "Sydney";
+        }
+        public function getLastName(){
+            return "Wilkes";
+        }
+    }
+
+?>
