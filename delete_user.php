0f858cbe74db4b08abfdbb38de53b5807574515a
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-10 09:14:25 -0700

    new

--- delete_user.php
+++ delete_user.php
@@ -2,7 +2,7 @@
     require_once('./header.php');
 ?>  
 
-<form method="POST" target="user_delete.php">
+<form method="POST" action="user_delete.php">
     Username: <input type="text" name="username"/><br/>
     <input type="submit" value="Eliminate User" />
 </form>
