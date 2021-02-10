d770d1baa7627ce62108f99f6845d759ac86f762
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-09 17:43:32 -0700

    new

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once('./session/session.php');
+
+    $session = new session();
+    $session->login("Gabe", "12345");
+?>  
--- user_view.php
+++ user_view.php
@@ -1,6 +1,6 @@
 <?php
     require_once('./header.php');
-?>
+?>  
 
 <?php
     require_once('./user/user.php');
@@ -9,10 +9,12 @@
     $user->getUser(1);
 
     echo $user->getUsername();
-    echo "<br/>";
-
+    echo "<br />";
+    echo $user->getFirstName();
+    echo "<br />";
+    echo $user->getLastName();
 ?>
 
 <?php
     require_once('./footer.php');
-?>
+?> 
