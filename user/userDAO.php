0d94b37f0b69483212a53323065c97a39a60901f
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-09 18:16:41 -0700

    new

--- user/userDAO.php
+++ user/userDAO.php
@@ -1,23 +1,9 @@
-
 <?php
-class UserDAO {
-  function getUser($user){
-    require_once('./utilities/connection.php');
-    
-    $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE user_id =" . $user->getUserId();
-    $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $user->setFirstName($row["first_name"]);
-        $user->setLastName($row["last_name"]);
-        $user->setUsername($row["username"]);
+class userDAO{
+    public function getUser($user){
+        $user->setUsername("sydneywilkes22");
     }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
-  }
 }
+
 ?>
