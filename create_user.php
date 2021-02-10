86ad300c53abb22113111cdd7b60f2d514df608b
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-09 17:57:54 -0700

    new

--- create_user.php
+++ create_user.php
@@ -2,6 +2,15 @@
     require_once('./header.php');
 ?>  
 
+<form method="POST" target="user_insert.php">
+    Username: <input type="text" name="username"/><br/>
+    Firstname: <input type="text" name="firstname"/><br/>
+    Lastname: <input type="text" name="lastname"/><br/>
+    Password: <input type="text" name="password"/><br/>
+    <input type="submit" value="Create User" />
+
+</form>
+
 
 <?php
     require_once('./footer.php');
