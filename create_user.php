ef2a7117c90f530b0d52ca101cdaacb490ad0b1b
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-09 17:59:30 -0700

    new

--- create_user.php
+++ create_user.php
@@ -6,7 +6,7 @@
     Username: <input type="text" name="username"/><br/>
     Firstname: <input type="text" name="firstname"/><br/>
     Lastname: <input type="text" name="lastname"/><br/>
-    Password: <input type="text" name="password"/><br/>
+    Password: <input type="password" name="password"/><br/>
     <input type="submit" value="Create User" />
 
 </form>
