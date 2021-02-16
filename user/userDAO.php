6a540d5c29a83d90444a83cfe5836ae7b08ec40c
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-16 14:09:05 -0700

    new

--- user/userDAO.php
+++ user/userDAO.php
@@ -3,15 +3,15 @@ class UserDAO {
   function getUser($user){
     require_once('./utilities/connection.php');
     
-    $sql = "SELECT FirstName, LastName, userName, user_id FROM user WHERE user_id =" . $user->getUserId();
+    $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE user_id =" . $user->getUserId();
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $user->setFirstName($row["FirstName"]);
-        $user->setLastName($row["LastName"]);
-        $user->setUsername($row["userName"]);
+        $user->setFirstName($row["first_name"]);
+        $user->setLastName($row["last_name"]);
+        $user->setUsername($row["username"]);
     }
     } else {
         echo "0 results";
@@ -26,10 +26,10 @@ class UserDAO {
     
     $sql = "INSERT INTO cs3620_proj.user
     (
-    `userName`,
+    `username`,
     `password`,
-    `FirstName`,
-    `LastName`)
+    `first_name`,
+    `last_name`)
     VALUES
     ('" . $user->getUsername() . "',
     '" . $user->getPassword() . "',
