3e4d52928cf875de9f67c18960e2ad205452e665
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-16 18:23:11 -0700

    new

--- setenv.php
+++ setenv.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 session_start();
 
-$_SESSION['SQLPW'] = "sydneysorensen@cs3620";
-$_SESSION['SQLUSER'] = "Volleyball13!";
+$_SESSION['SQLPW'] = "Volleyball13!";
+$_SESSION['SQLUSER'] = "sydneysorensen@cs3620";
 
 ?>
