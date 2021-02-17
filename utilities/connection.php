224ef70ffa975186a51b3978871d0595b37b7986
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-16 18:45:13 -0700

    new

--- utilities/connection.php
+++ utilities/connection.php
@@ -2,7 +2,7 @@
 $servername = "cs3620.mysql.database.azure.com";
 $username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
 $password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
-$dbname = "cs3620";
+$dbname = "cs3620_proj";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
