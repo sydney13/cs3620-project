295944c584d27be5c7b42c6f792ce118d47d688f
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-19 12:02:51 -0700

    new

--- user/userDAO.php
+++ user/userDAO.php
@@ -7,7 +7,6 @@ class UserDAO {
     // $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE username ='" . $user->getUserId() . "'";
     // $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE last_name ='" . $user->getUserId() . "'";
     // $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE first_name ='" . $user->getUserId() . "'";
-<<<<<<< HEAD
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -47,8 +46,6 @@ class UserDAO {
     require_once('./utilities/connection.php');
     
     $sql = "SELECT first_name, last_name, username, first_name FROM user WHERE first_name =\"" . $user->getFirstName() . "\"";
-=======
->>>>>>> 9714bd8fa492c42390aa3514eb1d788e1e06666e
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -64,7 +61,6 @@ class UserDAO {
     $conn->close();
   }
 
-<<<<<<< HEAD
 
   function getTheLastName($user){
     require_once('./utilities/connection.php');
@@ -72,13 +68,6 @@ class UserDAO {
     $sql = "SELECT first_name, last_name, username, last_name FROM user WHERE last_name = \"" . $user->getLastName() . "\"";
     $result = $conn->query($sql);
 
-=======
-  function getTheUserName($user){
-    require_once('./utilities/connection.php');
-    
-    $sql = "SELECT first_name, last_name, username, username FROM user WHERE username =\"" . $user->getUsername() . "\"";
-    $result = $conn->query($sql);
-
     if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -92,28 +81,6 @@ class UserDAO {
     $conn->close();
   }
 
-
-  function getTheFirstName($user){
-    require_once('./utilities/connection.php');
-    
-    $sql = "SELECT first_name, last_name, username, first_name FROM user WHERE first_name =\"" . $user->getFirstName() . "\"";
-    $result = $conn->query($sql);
-
->>>>>>> 9714bd8fa492c42390aa3514eb1d788e1e06666e
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $user->setFirstName($row["first_name"]);
-        $user->setLastName($row["last_name"]);
-        $user->setUsername($row["username"]);
-    }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
-  }
-
-<<<<<<< HEAD
   function checkLogin($passedinusername, $passedinpassword){
     require_once('./utilities/connection.php');
     $user_id = 0;
@@ -131,26 +98,6 @@ class UserDAO {
     }
     $conn->close();
     return $user_id;
-=======
-
-  function getTheLastName($user){
-    require_once('./utilities/connection.php');
-    
-    $sql = "SELECT first_name, last_name, username, last_name FROM user WHERE last_name = \"" . $user->getLastName() . "\"";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $user->setFirstName($row["first_name"]);
-        $user->setLastName($row["last_name"]);
-        $user->setUsername($row["username"]);
-    }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
->>>>>>> 9714bd8fa492c42390aa3514eb1d788e1e06666e
   }
 
 
