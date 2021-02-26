1da48a3a2861d296e381dbb31c7ce9c9dbcf5161
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-02-26 14:45:17 -0700

    new

---
+++ LoginPage/setenv.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$_SESSION['SQLPW'] = "Volleyball13!";
+$_SESSION['SQLUSER'] = "sydneysorensen@cs3620";
+
+?>
