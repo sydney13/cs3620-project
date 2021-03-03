4398479ff76b6b9fbc033876530eac8c3232b8e8
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-03-02 18:33:07 -0700

    new

--- LoginPage/config.php
+++ LoginPage/config.php
@@ -5,7 +5,7 @@ require 'setenv.php';
 $SERVER_NAME = 'cs3620.mysql.database.azure.com';
 $DATABASE_USER = $_SESSION['SQLUSER'];
 $DATABASE_PASS = $_SESSION['SQLPW'];
-$DATABASE_NAME = 'cs3620_proj';
+$DATABASE_NAME = 'cs3620SQL';
 
 $con = mysqli_connect($SERVER_NAME, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if(mysqli_connect_errno()) {
