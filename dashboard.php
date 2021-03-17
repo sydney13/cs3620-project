407a6ea40c6522efaa5ef6a510eddae9916c9e82
sydney13 <sydneysorensen13@users.noreply.github.com>
2021-03-17 08:14:14 -0600

    new

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 
     <!-- Begin page content -->
     <main role="main" class="container">
-      <h1 class="mt-5">Gabe's Favorite Shows</h1>
+      <h1 class="mt-5">Sydney's Favorite Shows</h1>
       
 
       <?php
@@ -30,19 +30,6 @@
                   <br />';
         }
       ?>
-
-
-      <!--
-     <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-        </div>
-     </div>
-    -->
 
     </main>
 
---
+++ showDAO.php
@@ -0,0 +1,35 @@
+<?php
+
+class ShowDAO{
+    function getAllShows(){
+        require_once('config.php');
+        require_once('show.php');
+
+        $sql = 'SELECT show_id, show_name, show_description, show_rating FROM W01283645.showss';
+        $result = $con->query($sql);
+
+        $shows;
+        $index = 0;
+
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                $show = new show();
+
+                $show->setShowId($row["show_id"]);
+                $show->setShowName($row["show_name"]);
+                $show->setShowDescription($row["show_description"]);
+                $show->setShowRating($row["show_rating"]);
+                $shows[$index] = $show;
+                $index++;
+            }
+        }
+        $con->close();
+
+        return $shows;
+    }
+}
+
+
+
+
+?>
